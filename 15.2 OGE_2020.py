d8afa2e400a2c8c114b4b010704aeb608e65a2fe
pandrey <pandrey76@yandex.ru>
2020-02-19 00:34:57 +0300

    OGE 2020. Task 15.2.
    Refactoring code

--- 15.2 OGE_2020.py
+++ 15.2 OGE_2020.py
@@ -12,7 +12,7 @@ min_enter_value = -1
 max_enter_value = 21
 
 
-def gettingInputValue(title_str, min_count, max_count):
+def getting_input_value(title_str, min_count, max_count):
     """
 
     :param title_str:
@@ -39,70 +39,76 @@ def gettingInputValue(title_str, min_count, max_count):
                     return num
 
 
-def constructValueList():
-    count = gettingInputValue(title_count_of_values, min_count_value, max_count_value)
+def construct_value_list():
+    count = getting_input_value(title_count_of_values, min_count_value, max_count_value)
     i = 0
     val = None
     is_correct_action_flag = False
     result_value_list = []
     while i != count:
-
         if not is_must_present_value:
-            val = gettingInputValue(title_of_values, min_enter_value, max_enter_value)
+            val = getting_input_value(title_of_values, min_enter_value, max_enter_value)
         else:
             if (i == count - 1) & (not is_correct_action_flag):
                 while not is_correct_action_flag:
-                    val = gettingInputValue(title_of_values, min_enter_value, max_enter_value)
-                    is_correct_action_flag = isCorrectValue(val)
+                    val = getting_input_value(title_of_values, min_enter_value, max_enter_value)
+                    is_correct_action_flag = is_correct_value(val)
             else:
-                val = gettingInputValue(title_of_values, min_enter_value, max_enter_value)
-                if isCorrectValue(val):
+                val = getting_input_value(title_of_values, min_enter_value, max_enter_value)
+                if is_correct_value(val):
                     is_correct_action_flag = True
-
         i += 1
         result_value_list.append(val)
 
     return result_value_list
 
 
-def isCorrectValue(int_val, ):
+def is_correct_value(int_val):
     if int_val >= 1:
         return True
     else:
         return False
 
 
-def gettingResult1(result_list):
+def getting_result1(result_list):
     value = 0
     for val in result_list:
-        if isCorrectValue(val):
+        if is_correct_value(val):
             if value < val:
                 value = val
-            
     return value
 
 
-def gettingResult2(result_list):
+def getting_result2(result_list):
     value = 0
     for val in result_list:
         if val == 0:
             return 'YES'
     return 'NO'
-            
 
 
-list_of_entering_value = constructValueList()
-print(gettingResult1(list_of_entering_value))
+list_of_entering_value = construct_value_list()
 
-print(gettingResult2(list_of_entering_value))
+print(getting_result1(list_of_entering_value))
+
+print(getting_result2(list_of_entering_value))
 
 
-# Min_value
-#   min_value = max_enter_value
-#    for val in result_list:
-#        if isCorrectValue(val):
-#            if val < min_value:
-#                min_value = val
-#    if min_value == max_enter_value:
-#        min_value = 0
-#    return min_value
+def getting_min_value(result_list):
+    min_value = max_enter_value
+    for val in result_list:
+        if is_correct_value(val):
+            if val < min_value:
+                min_value = val
+    if min_value == max_enter_value:
+        min_value = 0
+    return min_value
+
+
+def getting_max_value(result_list):
+    value = 0
+    for val in result_list:
+        if is_correct_value(val):
+            if val > value:
+                value = val
+    return value
